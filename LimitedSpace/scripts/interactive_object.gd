c799dde1fa5548632026cc6c756d145f3365dad5
Ricardo Bustamante <ricardo@busta.dev>
2023-10-02 09:21:56 +0200

    rename stuff

--- LimitedSpace/scripts/interactive_object.gd
+++ LimitedSpace/scripts/interactive_object.gd
@@ -7,9 +7,9 @@ const OUTLINE_LAYER_MASK: int = 2
 @export var object_name: String = ""
 @export var object_description: String = ""
 
-func set_outline(visible: bool):
+func set_outline(outline: bool):
     for child in get_children():
         if child is VisualInstance3D:
-            child.set_layer_mask_value(OUTLINE_LAYER_MASK, visible)
+            child.set_layer_mask_value(OUTLINE_LAYER_MASK, outline)
 
 
