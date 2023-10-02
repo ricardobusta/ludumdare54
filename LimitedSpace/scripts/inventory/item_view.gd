507ac63ca98ba5ef2cac088b7aad1d6a99d05a2a
Ricardo Bustamante <ricardo@busta.dev>
2023-10-02 09:26:26 +0200

    update item view

--- LimitedSpace/scripts/inventory/item_view.gd
+++ LimitedSpace/scripts/inventory/item_view.gd
@@ -2,5 +2,11 @@ extends Node
 
 class_name ItemView
 
-@export var icon: TextureRect = null
-@export var amount: Label = null
+@export var icon_texture_rect: TextureRect = null
+@export var amount_label: Label = null
+
+func set_amount(amount: int):
+    amount_label.text = "x%s" % amount
+
+func set_icon(texture: Texture2D):
+    icon_texture_rect.texture = texture
